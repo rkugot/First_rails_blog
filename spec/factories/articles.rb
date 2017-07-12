3ef18a2f1a66a6eb9de2a96afd42df294d261113
r.kugot <stream13k@gmail.com>
2017-07-12 16:42:47 +0300

    Add article_with_comments factory

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,5 +2,15 @@ FactoryGirl.define do
   factory :article do
     title "Article title"
     text "Article text"
+    #создаём фабрику с названием article_with_comments
+    #для создания стать с несколькими комментариями
+
+    factory :article_with_comments do
+      #после создания article
+      after :create do |article, evaluator|
+        #создаём список из комментариев (3) для созданного article из блока
+        create_list :comment, 3, article: article
+      end
+    end
   end
 end
