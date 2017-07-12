8908d3821331edd080a2a10c8199a0aee6e3c1cd
r.kugot <stream13k@gmail.com>
2017-07-12 16:16:31 +0300

    Add subject method to article and test it

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,8 @@ class Article < ApplicationRecord
   validates :title, presence: true
   validates :text, presence: true
   has_many :comments, dependent: :destroy
+
+  def subject
+    title
+  end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,7 +5,17 @@ describe Article do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
   end
+
   describe "associations" do
     it { should have_many :comments }
+  end
+
+  describe "#subject" do
+    it "returns the article title" do
+      #создаём объект article с title 'Lorem Ipsum'
+      article = create(:article, title: 'Lorem Ipsum')
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
   end
 end
