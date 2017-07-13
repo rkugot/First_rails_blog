bc25036a6d9a0f46c76126a424517f7fc81b85c0
r.kugot <stream13k@gmail.com>
2017-07-13 16:38:47 +0300

    Update create contact spec
    Test filling form

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 feature "Contact Creation" do
+
   scenario "allows access to contact page" do
     visit '/contacts'
     expect(page).to have_content I18n.t('contacts.contact_us')
   end
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+    fill_in 'Email', with: 'user@example.com'
+    fill_in 'Message', with: 'something'
+    click_button 'Send message'
+    expect(page).to have_content 'Thank you'
+  end
+
 end
